8176bb8120b4c0f634e7411f0654b1b3d2262a02
Genie16-github <4411dlwoqja@naver.com>
2023-02-24 12:43:49 +0900

    P120811 중앙값 구하기

---
+++ src/test/java/ll/Level0/P120811/MainTest.java
@@ -0,0 +1,19 @@
+package ll.Level0.P120811;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void t1() {
+        assertThat(new Solution().solution(new int[]{1,2,7,10,11})).isEqualTo(7);
+    }
+
+    @Test
+    void t2() {
+        assertThat(new Solution().solution(new int[]{9,-1,0})).isEqualTo(0);
+    }
+}
