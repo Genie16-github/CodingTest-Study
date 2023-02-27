2413f07a6fe6724a4c1afbddfd93ac663c6b2ce1
Genie16-github <4411dlwoqja@naver.com>
2023-02-27 14:52:37 +0900

    P120908 문자열안에 문자열

---
+++ src/main/java/ll/Level0/P120908/Main.java
@@ -0,0 +1,15 @@
+package ll.Level0.P120908;
+// 문자열안에 문자열
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int solution(String str1, String str2) {
+        String tmp = str1.replace(str2, "");
+        if(tmp.equals(str1)) return 2;
+        return 1;
+    }
+}
