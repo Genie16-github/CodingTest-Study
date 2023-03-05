472a824978fd9de63a5ddfb44457a00a3a278bc0
Genie16-github <4411dlwoqja@naver.com>
2023-03-06 08:43:47 +0900

    P120897 약수 구하기

---
+++ src/main/java/ll/Level0/P120897/Main.java
@@ -0,0 +1,20 @@
+package ll.Level0.P120897;
+
+import java.util.ArrayList;
+
+// 약수 구하기
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 1; i < n+1; i++){
+            if (n % i == 0) arr.add(i);
+        }
+        return arr.stream().sorted().mapToInt(e -> e).toArray();
+    }
+}
