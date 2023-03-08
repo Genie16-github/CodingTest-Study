4d308129f802c89aab2d4bf572e3b231414f950b
Genie16-github <4411dlwoqja@naver.com>
2023-03-08 11:55:51 +0900

    P120848 팩토리얼

---
+++ src/main/java/ll/Level0/P120848/Main.java
@@ -0,0 +1,22 @@
+package ll.Level0.P120848;
+// 팩토리얼
+public class Main {
+}
+
+class Solution {
+    public static int fac(int n) {
+        if(n == 1) return 1;
+        return n * fac(n-1);
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 10; i > 0; i--) {
+            if(fac(i) <= n) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
