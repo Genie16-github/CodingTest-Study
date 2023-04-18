a66b5277eb356e919e9fda39afbc278a26a916d8
Genie16-github <4411dlwoqja@naver.com>
2023-04-18 14:13:23 +0900

    P120902 문자열 계산하기

---
+++ src/main/java/ll/Level0/P120902/Main.java
@@ -0,0 +1,23 @@
+package ll.Level0.P120902;
+// 문자열 계산하기
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("3 + 4"));
+    }
+}
+
+class Solution {
+    public int solution(String my_string) {
+        int answer;
+        String[] arr = my_string.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("+")) {
+                arr[i+1] = String.valueOf(Integer.parseInt(arr[i-1]) + Integer.parseInt(arr[i+1]));
+            } else if (arr[i].equals("-")) {
+                arr[i+1] = String.valueOf(Integer.parseInt(arr[i-1]) - Integer.parseInt(arr[i+1]));
+            }
+        }
+        answer = Integer.parseInt(arr[arr.length-1]);
+        return answer;
+    }
+}
