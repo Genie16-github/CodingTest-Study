4a6101e2633a69f13380cc1d856f3cdbf75a9b45
Genie16-github <4411dlwoqja@naver.com>
2023-04-20 14:48:45 +0900

    P120808 분수의 덧셈

---
+++ src/main/java/ll/Level0/P120808/Main.java
@@ -0,0 +1,31 @@
+package ll.Level0.P120808;
+
+import java.util.Arrays;
+
+// 분수의 덧셈
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(
+                new Solution().solution(1, 2, 3, 4)));
+        System.out.println(Arrays.toString(
+                new Solution().solution(9, 2, 1, 3)));
+    }
+}
+
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer;
+        int bottom = denom1 * denom2;
+        int top = (numer1 * denom2) + (numer2 * denom1);
+        int n = gcd(top, bottom);
+        answer = new int[]{top / n, bottom / n};
+        return answer;
+    }
+
+    public int gcd(int a, int b){
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+}
