ce5c134334cc17f56210de6851d26beba892d19a
Genie16-github <4411dlwoqja@naver.com>
2023-04-24 13:52:30 +0900

    P12937 짝수와 홀수

---
+++ src/main/java/ll/Level1/P12937/Main.java
@@ -0,0 +1,15 @@
+package ll.Level1.P12937;
+// 짝수와 홀수
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(3));
+        System.out.println(new Solution().solution(4));
+    }
+}
+
+class Solution {
+    public String solution(int num) {
+        if (num % 2 == 0) return "Even";
+        return "Odd";
+    }
+}
