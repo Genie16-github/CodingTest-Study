d0ad03c23ff8fdac474c172fa75e5705b5d0621a
Genie16-github <4411dlwoqja@naver.com>
2023-04-24 14:21:24 +0900

    P12954 x만큼 간격이 있는 n개의 숫자

---
+++ src/main/java/ll/Level1/P12954/Main.java
@@ -0,0 +1,24 @@
+package ll.Level1.P12954;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// x만큼 간격이 있는 n개의 숫자
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().solution(2, 5)));
+        System.out.println(Arrays.toString(new Solution().solution(4, 3)));
+        System.out.println(Arrays.toString(new Solution().solution(-4, 2)));
+    }
+}
+
+class Solution {
+    public long[] solution(int x, int n) {
+        List<Long> list = new ArrayList<>();
+        for (int i = 1; i < n+1; i++) {
+            list.add((long)x*i);
+        }
+        return list.stream().mapToLong(e -> e).toArray();
+    }
+}
