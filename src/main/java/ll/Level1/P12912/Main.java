26773f5e6ecf0ad8f819a1d812ce9c215ffb3161
Genie16-github <4411dlwoqja@naver.com>
2023-04-25 10:16:20 +0900

    P12912 두 정수 사이의 합

---
+++ src/main/java/ll/Level1/P12912/Main.java
@@ -0,0 +1,24 @@
+package ll.Level1.P12912;
+// 두 정수 사이의 합
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(3, 5));
+        System.out.println(new Solution().solution(3, 3));
+        System.out.println(new Solution().solution(5, 3));
+    }
+}
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if (a > b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        for (int i = a; i < b+1; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
