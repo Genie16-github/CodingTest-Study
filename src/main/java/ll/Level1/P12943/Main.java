3f50a701d29386a761e67f39b5c0214869b7bd9f
Genie16-github <4411dlwoqja@naver.com>
2023-04-25 10:30:29 +0900

    P12943 콜라츠 추측

---
+++ src/main/java/ll/Level1/P12943/Main.java
@@ -0,0 +1,23 @@
+package ll.Level1.P12943;
+// 콜라츠 추측
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(6));
+        System.out.println(new Solution().solution(16));
+        System.out.println(new Solution().solution(626331));
+    }
+}
+
+class Solution {
+    public int solution(long num) {
+        if (num == 1) return 0;
+        for (int i = 0; i < 500; i++) {
+            if (num == 1){
+                return i;
+            }
+            if (num % 2 == 0) num /= 2;
+            else num = num * 3 + 1;
+        }
+        return -1;
+    }
+}
