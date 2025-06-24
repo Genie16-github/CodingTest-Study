a79e24a735189f361f38325e3bee1c69a5053d6e
Genie16-github <4411dlwoqja@naver.com>
2025-06-24 21:26:05 +0900

    브루트포스 알고리즘

---
+++ src/main/java/baekjoon/bronze/P2798.java
@@ -0,0 +1,45 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 블랙잭 (브2)
+public class P2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int res = cardSum(N, M, arr);
+
+        System.out.println(res);
+    }
+
+    public static int cardSum(int n, int m, int[] arr) {
+
+        int maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum <= m && sum > maxSum) maxSum = sum;
+                }
+            }
+        }
+
+        return maxSum;
+    }
+}
+
+
