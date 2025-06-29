a0c8d0ef49937508c253cf0b94df1c1b661cd29e
Genie16-github <4411dlwoqja@naver.com>
2025-06-29 20:41:55 +0900

    집합

---
+++ src/main/java/baekjoon/silver/P10815.java
@@ -0,0 +1,36 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+// 숫자 카드 (실5)
+public class P10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int k = Integer.parseInt(st.nextToken());
+            if (set.contains(k)) {
+                sb.append("1 ");
+            } else {
+                sb.append("0 ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
